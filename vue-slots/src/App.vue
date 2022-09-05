f06863ed46f7665802f3b48d81f311e43840ad66
Lucas Maranhao <maranhaoaquino@gmail.com>
2022-09-05 11:34:50 -0300

    teste

---
+++ vue-slots/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <modal-principal>
+      <template v-slot:header="{logo}">{{logo}}</template>
+      <template v-slot:default="{user}">
+        <h1>{{user.nome}}</h1>
+        <p>Esse é um teste</p>
+        <button>Comprar</button>
+      </template>
+      <template #footer>Esse é o footer</template>
+    </modal-principal>
+  </div>
+</template>
+
+<script>
+import ModalPrincipal from "./components/ModalPrincipal.vue";
+
+export default {
+  name: "app",
+  components: {
+    ModalPrincipal
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ vue-slots/src/components/ModalPrincipal.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="modal">
+        <slot name="header" :logo="logo"></slot>
+        <h1>{{user}}</h1>
+        <slot :user="user"></slot>
+        <slot name="footer"></slot>
+    </div>
+</template>
+
+<script>
+export default{
+    name: "ModalPrincipal",
+    data(){
+        return{
+            logo: "Techno",
+            user: {
+                nome: "André"
+            }
+        }
+    }
+}
+</script>
+
+<style>
+</style>
